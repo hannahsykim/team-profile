5f3722a23354c5b0ec1a29e392002adc0e775df2
hannahk <hanhannah839@gmail.com>
2022-12-03 23:49:14 -0800

    testing node

--- index.js
+++ index.js
@@ -111,5 +111,89 @@ function another() {
     
 };
 
+function managerCard(manager) {
+    return `
+    <div class = "col-auto mb-3">
+     <div class = "card" style="width: 18rem;">
+        <div class = "card-header">
+            <h4 class = "card-title">${manager.name}</h3>
+            <p class = "card-role">${manager.role}</h4>
+        </div>
+        <div class = "card-body">
+            <p class = "card-text">Id: ${manager.id}</p>
+            <p class = "card-text">Email: ${manager.email}</p>
+            <p class = "card-text">Office Number: ${manager.officeNumber}</p>
+        </div>
+     </div>
+    </div>
+    `;
+}
 
+function engineerCard(engineer) {
+    return `
+    <div class = "col-auto mb-3">
+     <div class = "card" style="width: 18rem;">
+        <div class = "card-header">
+            <h4 class = "card-title">${engineer.name}</h3>
+            <p class = "card-role">${engineer.role}</h4>
+        </div>
+        <div class = "card-body">
+            <p class = "card-text">Id: ${engineer.id}</p>
+            <p class = "card-text">Email: ${engineer.email}</p>
+            <p class = "card-text">Github: ${engineer.github}</p>
+        </div>
+     </div>   
+    </div>
+    `;
+}
+
+function internCard(intern) {
+    return `
+    <div class = "col-auto mb-3">
+     <div class = "card" style="width: 18rem;">
+        <div class = "card-header">
+            <h4 class = "card-title">${intern.name}</h3>
+            <p class = "card-role">${intern.role}</h4>
+        </div>
+        <div class = "card-body">
+            <p class = "card-text">Id: ${intern.id}</p>
+            <p class = "card-text">Email: ${intern.email}</p>
+            <p class = "card-text">School: ${intern.school}</p>
+        </div>
+     </div>  
+    </div>
+    `;
+}
+
+function renderHTMLFile() {
+    fs.writeFileSync('./index.html', /*html*/`
+    <html>
+        <head>
+            <title> Team Profile </title>
+            <link rel="stylesheet" href="styles.css">
+            <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css">
+        </head>
+        <body>
+            <div class = "container-fluid mt-4">
+                <div class = "justify-content-center mb-4">
+                      <h1 id="title">Team Profile</h1>
+                </div>
+                <div class = "row justify-content-center">
+                    ${employees.map((employee) => {
+                        switch (employee.getRole()) {
+                          case "Manager":
+                            return managerCard(employee);
+                          case "Engineer":
+                            return engineerCard(employee);
+                          case "Intern":
+                            return internCard(employee);
+                        }
+                      }
+                      ).join("")}   
+                </div>
+            </div>
+        </body>
+    </html>
+    `
+)};
 newEmployee();
